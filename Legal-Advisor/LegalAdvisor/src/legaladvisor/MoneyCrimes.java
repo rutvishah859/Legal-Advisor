362112049e3440e88020929e9afbb1b3181cff70
BaileyLuu <50964948+BaileyLuu@users.noreply.github.com>
2019-05-29 15:09:20 -0400

    I create the Civil class

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/MoneyCrimes.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/MoneyCrimes.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package legaladvisor;
 
-/**
- *
- * @author 325075141
- */
 public class MoneyCrimes extends CriminalCase {
     private double amtStolen; 
     private boolean violence;
     private String typeOfProperty; 
     private String crimeType; 
-    private String keyWords []=  {"fraud", "credit", "card", "mortgage", "trading", "tax", "theft", "stole"};
-    
+    //private String keyWords []=  {"fraud", "credit", "card", "mortgage", "trading", "tax", "theft", "stole"};
+    private String crimeTypes [] = {"fraud", "electronic crime", "money laundering", "theft"}; 
+    private String fraud [] = {"transfered funds", "investment", "bank"};  
+    private String electronicCrime [] = {"computer", "app", "phone", "online banking"};
+    private String moneyLaundering [] = {"fake money"}; 
+    private String theft [] = {"stole"}; 
+            
     public MoneyCrimes (String rem, String typ, String jur, String crim, String sen, double amtSt, boolean viol, String typeProp){
         super(rem,typ,jur,crim,sen);
         this.amtStolen = amtSt; 
@@ -23,8 +19,23 @@ public class MoneyCrimes extends CriminalCase {
         this.typeOfProperty = typeProp; 
     }
     
-    public void setCrimeAndSentence (){
+    public void setCrimeAndSentence (String keyword){
         String sentence; 
+        if (super.SearchMechanism(keyword, fraud, 0)){
+            this.crimeType = crimeTypes [0]; 
+        }
+        else if (super.SearchMechanism(keyword, electronicCrime, 0)){
+            this.crimeType = crimeTypes [1]; 
+        }
+        else if(super.SearchMechanism(keyword, moneyLaundering, 0)){
+            this.crimeType = crimeTypes [2]; 
+        }
+        else if (super.SearchMechanism(keyword, theft, 0)){
+            this.crimeType = crimeTypes [3]; 
+        }
+        else {
+            crimeType = "not a crime against person"; 
+        }
         
     }
 
