acbbe16e57a8fa116f7f1f4cc19f79cca5e82530
Rutvi Shah <32719043+rutvishah859@users.noreply.github.com>
2019-05-29 14:58:31 -0400

    Update OHRC.java

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/OHRC.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/OHRC.java
@@ -9,6 +9,44 @@ package legaladvisor;
  *
  * @author 325075141
  */
-public class OHRC {
+public class OHRC extends HumanRights{
+    private String grounds; 
+    private String area; 
+    private String typeOfDis; 
+    
+    public OHRC(String rem, String typ, String jur, String gr, String ar, String typeDis) {
+        super(rem, typ, jur);
+        this.grounds = gr; 
+        this.area = ar; 
+        this.typeOfDis = typeDis; 
+    } 
+
+    public String getGrounds() {
+        return grounds;
+    }
+
+    public void setGrounds(String grounds) {
+        this.grounds = grounds;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getTypeOfDis() {
+        return typeOfDis;
+    }
+
+    public void setTypeOfDis(String typeOfDis) {
+        this.typeOfDis = typeOfDis;
+    }
+    
+    String printInfo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
