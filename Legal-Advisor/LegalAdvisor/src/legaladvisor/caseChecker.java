35353c8d5d45c6b23bffdf3d00f12ee07d59a828
Rutvi Shah <32719043+rutvishah859@users.noreply.github.com>
2019-05-30 14:16:26 -0400

    Update caseChecker.java

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/caseChecker.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/caseChecker.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package legaladvisor;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,9 +11,16 @@ public class caseChecker extends Case{
     private String Case; 
     private Case cal; 
     private String keyWord;
+    private File caseFile; 
     
-    public caseChecker(String rem, String typ, String jur) {
+    public caseChecker(String rem, String typ, String jur,File complaint) {
         super(rem, typ, jur);
+        caseFile = new File ("H:\\GitHub\\Legal-Advisor\\Legal-Advisor\\LegalAdvisor\\src\\legaladvisor");
+        try { 
+            BufferedReader br =  new BufferedReader (new FileReader (caseFile));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(caseChecker.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     String printInfo() {
