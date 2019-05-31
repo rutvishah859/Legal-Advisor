b6ba460dd97a47a4f04d5b2012626d16951a1d42
Rutvi Shah <32719043+rutvishah859@users.noreply.github.com>
2019-05-31 14:50:16 -0400

    moved all the keywords of MoneyCrimes to the Case class (to make it avalible in the parent class)

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/MoneyCrimes.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/MoneyCrimes.java
@@ -5,12 +5,6 @@ public class MoneyCrimes extends CriminalCase {
     private String typeOfProperty; 
     private String crimeType; 
     //private String keyWords []=  {"fraud", "credit", "card", "mortgage", "trading", "tax", "theft", "stole"};
-    private String crimeTypes [] = {"fraud", "robbery", "money laundering", "theft"};
-    private String fraud [] = {"transfered funds", "investment", "bank","computer", "app", "phone", "online banking"};    
-    private String electronicCrime [] = {"computer", "app", "phone", "online banking"};
-    private String moneyLaundering [] = {"fake money"}; 
-    private String theft [] = {"stole", "took", "does not own"}; 
-    private String robbery [] = {"forced","stole","broke in","threatened","forcefully stole"}; 
     
             
     public MoneyCrimes (String rem, String typ, String jur, String crim, String sen, double amtSt, boolean viol, String typeProp){
@@ -21,19 +15,19 @@ public class MoneyCrimes extends CriminalCase {
     }
     
     public void setCrimeAndSentence (String keyword){
-        if (super.SearchMechanism(keyword, fraud, 0)){
-            this.crimeType = crimeTypes [0]; 
+        if (super.SearchMechanism(keyword, super.getFraud(), 0)){
+            this.crimeType = super.getCrimeTypes() [0]; 
         }
-        else if (super.SearchMechanism(keyword, electronicCrime, 0)){
-            this.crimeType = crimeTypes [1]; 
+        else if (super.SearchMechanism(keyword, super.getElectronicCrime(), 0)){
+            this.crimeType = super.getCrimeTypes() [1]; 
         }
-        else if(super.SearchMechanism(keyword, moneyLaundering, 0)){
-            this.crimeType = crimeTypes [2]; 
+        else if(super.SearchMechanism(keyword, super.getMoneyLaundering(), 0)){
+            this.crimeType = super.getCrimeTypes() [2]; 
         }
-        else if (super.SearchMechanism(keyword, theft, 0)){
-            this.crimeType = crimeTypes [3]; 
-        if (super.SearchMechanism(keyword, fraud, 0)){
-            this.crimeType = crimeTypes [0]; 
+        else if (super.SearchMechanism(keyword, super.getTheft(), 0)){
+            this.crimeType = super.getCrimeTypes() [3]; 
+        if (super.SearchMechanism(keyword, super.getFraud(), 0)){
+            this.crimeType = super.getCrimeTypes() [0]; 
             if(amtStolen<5000){
             super.setSentence("Maximum Sentence is 6 months in jail or a 5000$ fine.");
             }
@@ -41,16 +35,16 @@ public class MoneyCrimes extends CriminalCase {
             super.setSentence("Minimum Sentence is 2 years in jail and the Maximum is 14 years in jail.");
             }
         }
-        else if (super.SearchMechanism(keyword, robbery, 0)){
-            this.crimeType = crimeTypes [1]; 
+        else if (super.SearchMechanism(keyword, super.getRobbery(), 0)){
+            this.crimeType = super.getCrimeTypes() [1]; 
             super.setSentence("Minimum sentence is 5 years in jail.");
         }
-        else if(super.SearchMechanism(keyword, moneyLaundering, 0)){
-            this.crimeType = crimeTypes [2]; 
+        else if(super.SearchMechanism(keyword, super.getMoneyLaundering(), 0)){
+            this.crimeType = super.getCrimeTypes() [2]; 
             super.setSentence("Minimum sentence is 6 months in jail or a 5000$ fine and the Maximum Sentence is 10 years.");
         }
-        else if (super.SearchMechanism(keyword, theft, 0)){
-            this.crimeType = crimeTypes [3]; 
+        else if (super.SearchMechanism(keyword, super.getTheft(), 0)){
+            this.crimeType = super.getCrimeTypes() [3]; 
             super.setSentence("Maximum sentence is 10 years in jail.");
 
         }
