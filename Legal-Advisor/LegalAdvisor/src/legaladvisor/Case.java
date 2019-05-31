a7666201a24ff39e9b45bb038f00aa5a0f9cd56a
Margarita21552 <50964952+Margarita21552@users.noreply.github.com>
2019-05-31 15:11:52 -0400

    Update Case.java

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/Case.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/Case.java
@@ -22,7 +22,8 @@ package legaladvisor;
     //OHRC keywords
     private String groundKeywords[]={"black","white","asian","muslim","jewish","christian","buddhist","hindu","female","male","man","woman","girl","boy","pregnancy","citizenship","culture","disabled","mental disability","physical disability","gay","homosexual","lesbian","bisexual","transgender","heterosexual","married","not married","kids","no kids","too young","too old","wellfare"};
     private String areaKeywords[]={"rent","house","work","employment","hired","fired","member","group","union","hospital","restaurant","movie theater","school","service","place","contract"};
-    
+    //Charter keywords
+    private String issues[]={"muslim","jewish","christian","buddhist","hindu","speech","expression","associate","group","non-violent protest","vote","election","parliament meeting","5","travel","into Canada","within Canada","out of Canada","safety","life","privacy","arrest","search","seizure","arrest","detention"};
     public Case(String rem, String typ, String jur){
         this.remedies = rem;
         this.type = typ;
