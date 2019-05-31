25bff54f93ad86ed768836dc6886f164de130ae9
Margarita21552 <50964952+Margarita21552@users.noreply.github.com>
2019-05-31 14:27:03 -0400

    fixed bug

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/Tester.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/Tester.java
@@ -8,11 +8,14 @@ public class Tester {
         System.out.println(c1.getMensRea());
         c1.setCrimeTypeAndSentence("killed");
         System.out.println(c1.getCrimeType());
-        
-        File c = new File ("H:\\GitHub\\Legal-Advisor\\Legal-Advisor\\LegalAdvisor\\src\\legaladvisor\\caseFile.txt");
+        OHRC o1=new OHRC("","","","","","");
+        o1.setGrounds("jewish");
+        System.out.println(o1.getGrounds());
+        /*File c = new File ("H:\\GitHub\\Legal-Advisor\\Legal-Advisor\\LegalAdvisor\\src\\legaladvisor\\caseFile.txt");
         caseChecker cc = new caseChecker ("abc", "abc", "abc", c);
         System.out.println(cc.printInfo()); 
         String word = "is"; 
-        System.out.println(cc.findKeyWords(cc ,word));
+        System.out.println(cc.findKeyWords(cc ,word));*/
+        
     }
 }
