c1503ca5459041bfc123c797209a95f3d09e3bbe
Margarita21552 <50964952+Margarita21552@users.noreply.github.com>
2019-05-31 15:00:40 -0400

    Update Charter.java

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/Charter.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/Charter.java
@@ -1,8 +1,7 @@
 package legaladvisor;
 
 public class Charter extends HumanRights {
-    private String right; 
-        
+    private String right;    
     public Charter(String rem, String typ, String jur, String r) {
         super(rem, typ, jur);
         this.right = r; 
