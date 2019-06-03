f631e3758744b0e4d2e49e5ffcf4e02ee4b5db4e
BaileyLuu <50964948+BaileyLuu@users.noreply.github.com>
2019-06-03 14:45:46 -0400

    Update Contract.java

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/Contract.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/Contract.java
@@ -8,7 +8,33 @@ public class Contract extends Civil {
     
     public Contract(String rem, String typ, String jur, String tC,String pur, boolean pC, boolean hC) {
         super(rem, typ, jur, tC);
-        
+        this.purpose = pur;
+        this.partiesConsent = pC;
+        this.hasContract = hC;
+    }
+    
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    public boolean isPartiesConsent() {
+        return partiesConsent;
+    }
+
+    public void setPartiesConsent(boolean partiesConsent) {
+        this.partiesConsent = partiesConsent;
+    }
+
+    public boolean isHasContract() {
+        return hasContract;
+    }
+
+    public void setHasContract(boolean hasContract) {
+        this.hasContract = hasContract;
     }
     
 }
