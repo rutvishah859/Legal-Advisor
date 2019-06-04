223fc3e9da7a7850f65408e585e3be67739fcd09
Margarita21552 <50964952+Margarita21552@users.noreply.github.com>
2019-06-04 15:12:12 -0400

    Created person

---
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/Person.java
@@ -0,0 +1,10 @@
+package legaladvisor;
+public class Person {
+private String name;
+private String complaintFilePath;
+private String phoneNum;
+private Account a1;
+public Person(String n, String c, String pn){
+    
+}
+}
