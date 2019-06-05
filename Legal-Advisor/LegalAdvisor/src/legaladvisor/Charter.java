c962110e388df2a8b7b95843041f78e76b417fef
Margarita21552 <50964952+Margarita21552@users.noreply.github.com>
2019-06-05 15:12:27 -0400

    updated

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/Charter.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/Charter.java
@@ -22,50 +22,50 @@ public class Charter extends HumanRights {
             else if(this.getISM()==7||this.getISM()==8){
                 this.right="Section 2 d): Freedom of Association";
             }
-            else if(this.getISM()==9){
+            else if(this.getISM()==9||this.getISM()==10){
                 this.right="Section 2 c):Freedom of peaceful assembly.";
             }
-            else if(this.getISM()==10){
+            else if(this.getISM()==11){
                 this.right="Section 3: Democratic rights";
             }
-            else if(this.getISM()==11||this.getISM()==12){
+            else if(this.getISM()==12||this.getISM()==13){
                 this.right="Section 4:Democratic rights";
             }
-            else if(this.getISM()>=13&&this.getISM()<16){
+            else if(this.getISM()>=15&&this.getISM()<18){
                 this.right="Section 5: Democratic rights";
             }
-            else if(this.getISM()>=16&&this.getISM()<20){
+            else if(this.getISM()>=18&&this.getISM()<24){
                 this.right="Section 6: Mobility Rights";
             }
-            else if(this.getISM()>=20&&this.getISM()<23){
+            else if(this.getISM()>=24&&this.getISM()<27){
                 this.right="Section 7: Life, Liberty, Security.";
                 super.setRemedies("Remedies are: Stay of proceedings, Reading in a term, Reading Down a term, Striking down a law, Excluding evidence in trial,Monetary Compensation");
             }
-            else if(this.getISM()>=23&&this.getISM()<25){
+            else if(this.getISM()>=27&&this.getISM()<29){
                 this.right="Section 8: Unreasonable Search and Seizure";
                 super.setRemedies("Remedies are: Stay of proceedings, Reading in a term, Reading Down a term, Striking down a law, Excluding evidence in trial,Monetary Compensation");
             }
-            else if(this.getISM()>=25&&this.getISM()<32){
+            else if(this.getISM()>=29&&this.getISM()<36){
                 this.right="Section 9 and 10: Arrest and Detention Rights";
                 super.setRemedies("Remedies are: Stay of proceedings, Reading in a term, Reading Down a term, Striking down a law, Excluding evidence in trial,Monetary Compensation");
             }
-            else if(this.getISM()>=32&&this.getISM()<38){
+            else if(this.getISM()>=36&&this.getISM()<41){
                 this.right="Section 11: Proceedings in Criminal and Penal matters.";
                 super.setRemedies("Remedies are: Stay of proceedings, Reading in a term, Reading Down a term, Striking down a law, Excluding evidence in trial,Monetary Compensation");
             }
-            else if(this.getISM()>=38&&this.getISM()<41){
+            else if(this.getISM()>=41&&this.getISM()<44){
                 this.right="Section 12: Cruel and Unusual Punishment";
                 super.setRemedies("Remedies are: Stay of proceedings, Reading in a term, Reading Down a term, Striking down a law, Excluding evidence in trial,Monetary Compensation");
             }
-            else if(this.getISM()>=41&&this.getISM()<43){
+            else if(this.getISM()>=44&&this.getISM()<46){
                 this.right="Section 13: Witness Rights";
                 super.setRemedies("Remedies are: Stay of proceedings, Reading in a term, Reading Down a term, Striking down a law, Excluding evidence in trial,Monetary Compensation");
             }
-            else if(this.getISM()==43){
+            else if(this.getISM()==46){
                 this.right="Section 14: Right to interpreter";
                 super.setRemedies("Remedies are: Stay of proceedings, Reading in a term, Reading Down a term, Striking down a law, Excluding evidence in trial,Monetary Compensation");
             }
-            else if(this.getISM()==44){
+            else if(this.getISM()==47){
                 this.right="Section 15: Equality Rights";
             }
         }
