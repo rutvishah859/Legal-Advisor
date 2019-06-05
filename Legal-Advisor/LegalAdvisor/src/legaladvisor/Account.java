1535806675b9905b62dce7653844d770aa99795f
Margarita21552 <50964952+Margarita21552@users.noreply.github.com>
2019-06-05 15:12:52 -0400

    updated

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/Account.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/Account.java
@@ -1,4 +1,14 @@
 package legaladvisor;
+import java.io.*;
 public class Account {
-    //private CaseChecker c1= new CaseChecker("","","",);
+    private Case c1;
+    private Lawyer l1;
+    private File f;
+    private caseChecker c;
+    public Account(String fileName){
+        f=new File(fileName);
+    }
+    public void setCaseChecker() throws IOException{
+        c=new caseChecker("","","",f);
+    }
 }
--- Legal-Advisor/LegalAdvisor/src/legaladvisor/Tester.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/Tester.java
@@ -1,8 +1,13 @@
 package legaladvisor;
 import java.io.*;
-
+import java.util.Scanner;
 public class Tester {
     public static void main(String []args) throws IOException{
+        Scanner input = new Scanner(System.in);
+        String filename;
+        System.out.println("Please enter a file name with your complaint. Please include the double slashes.");
+        filename=input.nextLine();
+        Person p1=new Person("Max", filename, "1-888-467-4999");
         CrimeAgainstPerson c1= new CrimeAgainstPerson("None","Criminal","Ontario","Crime Against The Person","null","indictable");
         c1.setMensRea("accident");
         System.out.println(c1.getMensRea());
