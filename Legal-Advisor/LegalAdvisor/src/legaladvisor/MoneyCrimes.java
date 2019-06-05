bfa913a478825aa37c4aa53f50fb8f57c7390449
Rutvi Shah <32719043+rutvishah859@users.noreply.github.com>
2019-06-05 15:11:25 -0400

    Update MoneyCrimes.java

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/MoneyCrimes.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/MoneyCrimes.java
@@ -15,18 +15,18 @@ public class MoneyCrimes extends CriminalCase {
     
     public void setCrimeAndSentence (String keyword){
         if (super.SearchMechanism(keyword, super.getFraud(), 0)){
-            this.crimeType.add = super.getCrimeTypes1() [0]; 
+            this.crimeType.add(super.getCrimeTypes1() [0]); 
         }
         else if (super.SearchMechanism(keyword, super.getElectronicCrime(), 0)){
-            this.crimeType.add = super.getCrimeTypes1() [1]; 
+            this.crimeType.add(super.getCrimeTypes1() [1]); 
         }
         else if(super.SearchMechanism(keyword, super.getMoneyLaundering(), 0)){
-            this.crimeType.add = super.getCrimeTypes1() [2]; 
+            this.crimeType.add(super.getCrimeTypes1() [2]); 
         }
         else if (super.SearchMechanism(keyword, super.getTheft(), 0)){
-            this.crimeType.add = super.getCrimeTypes1() [3]; 
+            this.crimeType.add (super.getCrimeTypes1() [3]); 
         if (super.SearchMechanism(keyword, super.getFraud(), 0)){
-            this.crimeType.add = super.getCrimeTypes1() [0]; 
+            this.crimeType.add (super.getCrimeTypes1() [0]); 
             if(amtStolen<5000){
             super.setSentence("Maximum Sentence is 6 months in jail or a 5000$ fine.");
             }
@@ -35,20 +35,20 @@ public class MoneyCrimes extends CriminalCase {
             }
         }
         else if (super.SearchMechanism(keyword, super.getRobbery(), 0)){
-            this.crimeType.add = super.getCrimeTypes1() [1]; 
+            this.crimeType.add (super.getCrimeTypes1() [1]); 
             super.setSentence("Minimum sentence is 5 years in jail.");
         }
         else if(super.SearchMechanism(keyword, super.getMoneyLaundering(), 0)){
-            this.crimeType.add = super.getCrimeTypes1() [2]; 
+            this.crimeType.add (super.getCrimeTypes1() [2]); 
             super.setSentence("Minimum sentence is 6 months in jail or a 5000$ fine and the Maximum Sentence is 10 years.");
         }
         else if (super.SearchMechanism(keyword, super.getTheft(), 0)){
-            this.crimeType.add = super.getCrimeTypes1() [3]; 
+            this.crimeType.add (super.getCrimeTypes1() [3]); 
             super.setSentence("Maximum sentence is 10 years in jail.");
 
         }
         else {
-            this.crimeType.add = "not a crime against person"; 
+            this.crimeType.add ("not a crime against person"); 
         }
         
     }
