6dca8474548000869f3eb4635fde21ffd45c090b
BaileyLuu <50964948+BaileyLuu@users.noreply.github.com>
2019-06-08 14:33:15 -0400

    Update Contract.java

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/Contract.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/Contract.java
@@ -5,6 +5,7 @@ public class Contract extends Civil {
     private String purpose; //contract must have purpose
     private boolean partiesConsent; //parties must agree the terms of contract
     private boolean capacity; // parties must have ability to understand what they were agreeing to
+    //                                0            1                  2                 3                4
     private String remedies [] = {"Damages" , "Injunctions", "Privity of Contract","Rescission","Specific Performance"};
     //remedies are available if the breach of contract occurs
     //Damages is awarded to compensate the injured party in the contract. Liquidates damages, sum of money specified in contract to settle the breach
@@ -21,6 +22,7 @@ public class Contract extends Civil {
     private boolean offer;
     private String unlawfulPurpose[] = {"betting", "gambling", "gaming"}; //the contract must have lawful objective or purpose
     private boolean lawfulPurpose;
+    //                                            0                 1                        2                      3
     private String dischargingContract [] = {"performance", "mutual agreement" , "frustration of contract", "breach of contract"};
     //breach of contract is failing to perform an obligation owed to another party
     //if breach occurs, it allows the party to cancel or end the contract
@@ -31,6 +33,7 @@ public class Contract extends Civil {
         this.purpose = pur;
         this.partiesConsent = pC;
         this.hasContract = hC;
+        this.participantAge = partAge;
         
     }
     
@@ -77,11 +80,11 @@ public class Contract extends Civil {
         this.hasContract = hasContract;
     }
     
-    public void setCapacity(boolean d, int a){
-        if((d == true) || (a < 19)){
-           this.capacity = true;
+    public void setCapacity(boolean dis, int age){
+        if((dis == true) || (age < 19)){    //if the person is disability or under 19 years old
+           this.capacity =false;            //the person does not have capacity to perform the contract
         }else{
-           this.capacity = false;
+           this.capacity = true;            //else the person have ability to perform the contract
         }
     }
     
