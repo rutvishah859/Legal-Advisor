daf84e41a215e2e52594660f808a8a1498262508
BaileyLuu <50964948+BaileyLuu@users.noreply.github.com>
2019-06-08 15:09:30 -0400

    Update Contract.java

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/Contract.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/Contract.java
@@ -6,17 +6,20 @@ public class Contract extends Civil {
     private boolean partiesConsent; //parties must agree the terms of contract
     private boolean capacity; // parties must have ability to understand what they were agreeing to
     //                                0            1                  2                 3                4
-    private String remedies [] = {"Damages" , "Injunctions", "Privity of Contract","Rescission","Specific Performance"};
+    private String remedies [] = {"Damages" , "Injunctions", "Rescission","Specific Performance"};
     //remedies are available if the breach of contract occurs
     //Damages is awarded to compensate the injured party in the contract. Liquidates damages, sum of money specified in contract to settle the breach
     //Specific performance is the court may order the party to fulfill the terms of a contract
     //Injuction: one party is ordered not to fulfill the terms of a contract
     //Rescission is when the court may order the contract to be cancelled
+    private String remedy;
+    private boolean failingToPerform;
     private boolean disability; // anyone with disability or impaired judgment can't make a valid contract
     private int participantAge; // anyone who under 18 or 19 years old does not have the capacity to enter into valid contract
     private boolean hasContract;    //must have contract in order to make a case
     private boolean brokenContract; // if the parties break the contract
-    private String contractWord []= {"agree","break","car","condition","contract","disagree","goods", "house","illness","make", "mental","negotiate","offer", "request"};
+    //                                  0       1      2        3            4          5          6         7         8        9       10       11       12        13        14      15       16
+    private String contractWord []= {"agree","break","car","compensate","condition","contract","disagree","failing","goods", "house","illness","make", "mental","negotiate","offer", "pay","request"};
     private String consideration;   //The actual value or amount exchanged between the two parties
     private boolean consider;
     private boolean offer;
@@ -44,21 +47,31 @@ public class Contract extends Civil {
             if((this.capacity == false) || (this.lawfulPurpose == false) || this.dischargingContract.equals(dischargingContract[2])){
                 typeContract = ("This is a void contract means the contract is no longer valid.");
             }else if(((this.partiesConsent == true) && (this.consider == true) && (this.lawfulPurpose == true) && (this.capacity == true) && (this.offer == true)) ||
-                    (keyword.equalsIgnoreCase(contractWord[2])) || keyword.equalsIgnoreCase(contractWord[7])){
+                    (keyword.equalsIgnoreCase(contractWord[2])) || keyword.equalsIgnoreCase(contractWord[9])){
                 typeContract = ("This is an expressed contracts because both parties are understand the terms of contract with legal purpose");
             }
         }
     }
     public void setRemedies(String keyword){
-        if(this.breachOfContract == true){
-            if(keyword.equalsIgnoreCase(contractWord[1])){
-                
+        if(this.failingToPerform == true){
+            if(this.breachOfContract == true){
+                if(keyword.equalsIgnoreCase(contractWord[3]) || keyword.equalsIgnoreCase(contractWord[15])){
+                    super.setRemedies("If one party is failling to perform, the judge will put this as compensatory damages to cover the loss of nonbreaching party.");
+                    this.remedy = this.remedies[0];
+                }else{
+                    super.setRemedies("If one party is failing to perform, the judge will one of these remedies:\n"
+                            + "1. The court may order the party to fulfill the terms of a contract\n"
+                            + "2. One party is not to fulfill the terms of a contract\n"
+                            + "3. the court may order the contract to be cancelled");
+                }
             }
         }
     }
     
-    public void setBreachOfContract(){
-        
+    public void setBreachOfContract(String keyword){
+        if(keyword.equalsIgnoreCase(contractWord[1]) || keyword.equalsIgnoreCase(contractWord[6])){
+            this.failingToPerform = true;
+        }
     }
     
     public String getPurpose() {
