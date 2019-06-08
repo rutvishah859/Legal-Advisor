f6ebe4129decc6b6f485fe7699086fba4bf01753
BaileyLuu <50964948+BaileyLuu@users.noreply.github.com>
2019-06-08 10:54:47 -0400

    fix

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/Civil.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/Civil.java
@@ -4,7 +4,7 @@ public abstract class Civil extends Case{
 
     String typeCase;
     public Civil(String rem, String typ, String jur, String tC) {
-        super(rem, typ, jur);
+        super(rem, jur);
        this.typeCase= tC;
     }
     public String getTypeCase(){
