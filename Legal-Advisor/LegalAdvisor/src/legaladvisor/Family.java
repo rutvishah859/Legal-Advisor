3783ca0805f12d04c99099edf64e28e6f0d36601
Margarita21552 <50964952+Margarita21552@users.noreply.github.com>
2019-06-11 15:42:56 -0400

    Update Family.java

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/Family.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/Family.java
@@ -5,11 +5,17 @@ public class Family extends Civil {
     private boolean marriage;
     private boolean child;
     private int numChild;
+    private String sentence;
     //                                0        1        2
     private String custodyType[] = {"full", "joint", "split"};
-    private String custodySentence;
     private boolean childDecision;
     private boolean parentDecision;
+    //                                     0         1            2           3      4           5
+    private String emotionalAbuse [] = {"hurting","insults","intimadation", "fun","threats", "yelling"};
+    //                                  0             1         2             3
+    private String sexualAbuse[] = {"harrassment","touching","unsafe","without consent"};
+    //                                   0          1          2         3         4          5          6          7         8         9           10   
+    private String physicalAbuse[] = {"burning", "choking", "cutting","hitting","locking","pinching","punching","pushing","slapping","throwing","objects"};
     private String abuseType;
     //                                        0           1         2
     private String abuseViolenceType[] = {"emotional","physical","sexual"};
@@ -17,35 +23,27 @@ public class Family extends Civil {
         super(rem, jur, tC);
         this.marriage = mar;
         this.child = child;
-        
     }
-    //this method will set what type of custory the marriage couple will have if they have child/children
-    //it will search for the famkeyword and check if the words relate to full, joint, or split custody
-    //if the marriage couple do not have children, they might suffer from Family Violence
     public void setCustody(String keyword){
-        String sentence;
         if(super.SearchMechanism(keyword, super.getFamKeyword(), 0)){  
             if((this.marriage == true) && (this.child == true)){
                 if((keyword.equalsIgnoreCase(super.getFamKeyword()[10]) || keyword.equalsIgnoreCase(super.getFamKeyword()[5]))){
-                    custodySentence = this.custodyType[0]; 
-                    sentence = "If both parents disagree to live together, this will be full custody means only one parent has the right to make all decision about the child";  
+                    this.sentence = "If both parents disagree to live together, this will be full custody means only one parent has the right to make all decision about the child";  
                 }else if(keyword.equalsIgnoreCase(super.getFamKeyword()[0]) || keyword.equalsIgnoreCase(super.getFamKeyword()[6])){
-                     sentence = "This is a joint custody means both parents have to share equal responsibility for child's wellfare."
+                     this.sentence = "This is a joint custody means both parents have to share equal responsibility for child's wellfare."
                     +  "The major decisions will be decided by both parents.";
                 }else if(this.numChild > 2){
                     if(this.childDecision == true){
-                        custodySentence = this.custodyType[1];
-                        sentence = "The child decision might affect whether this will be a split custody. The judge might take into consideration.";
+                        this.sentence = "The child decision might affect whether this will be a split custody. The judge might take into consideration.";
                     }else{
-                        custodySentence = this.custodyType[2];
-                        sentence = "This is a split custody";
+                        this.sentence = "This is a split custody";
                     }
                 }
            }
            if(keyword.equalsIgnoreCase(super.getFamKeyword()[9]) || keyword.equalsIgnoreCase(super.getFamKeyword()[0]) || keyword.equalsIgnoreCase(super.getFamKeyword()[7])
               || keyword.equalsIgnoreCase(super.getFamKeyword()[11]) || keyword.equalsIgnoreCase(super.getFamKeyword()[13])){
            //if keyword equals to "mistreatment", "abuse", "emotion", "sexual", "physical"
-              sentence = "This is a Family Violence case."; 
+              this.sentence = "This is a Family Violence case."; 
            }
         }
     }
@@ -53,28 +51,24 @@ public class Family extends Civil {
     public boolean getMarriage() {
         return marriage;
     }
-    //this method will determine what types of violence do the adult victim suffer
-    //it will compare and match the keyword if it's emotion, physical, or sexual abuse
    public void setFamilyViolenceType(String keyword){
        String sentence;
-       //the Family Violence only sets for the marriage couple that do not have child/children
-       //This is an adult violence
-       if((this.marriage == true) && (this.child == false)){    
              if(keyword.equalsIgnoreCase(super.getFamKeyword()[0])){
-                if(super.SearchMechanism(keyword, super.getEmotionalAbuse(), 0)){
+                if(super.SearchMechanism(keyword, emotionalAbuse, 0)){
                 abuseType = abuseViolenceType[0];
-                sentence = "This is a Family Violence case and you have suffered emotional abuse";
-                }else if(super.SearchMechanism(keyword, super.getPhysicalAbuse(), 0)){
+                this.sentence = "This is a Family Violence case and you have suffered emotional abuse";
+                }else if(super.SearchMechanism(keyword, physicalAbuse, 0)){
                 abuseType = abuseViolenceType[1];   
-                sentence = "This is a Family Violence case and you have suffered physical abuse";
-                }else if(super.SearchMechanism(keyword, super.getSexualAbuse(), 0)){
+                this.sentence = "This is a Family Violence case and you have suffered physical abuse";
+                }else if(super.SearchMechanism(keyword, sexualAbuse, 0)){
                 abuseType = abuseViolenceType[2];
-                sentence = "This is a Family Violence case and you have suffered sexual abuse";
-                }else{
-                abuseType = "You don't have any violence in family.";
+                this.sentence = "This is a Family Violence case and you have suffered sexual abuse";
+                }
+                else{
+                    abuseType="None";
                 }
             }
-       }
+       
    }
 
     public void setMarriage(boolean marriage) {
@@ -84,16 +78,15 @@ public class Family extends Civil {
     public boolean getChild() {
         return child;
     }
-    //it will return true if the marriage couple has child, return false if they don't have child
+
     public void setChild(boolean child) {
         this.child = child;
     }
-     
+    
     public boolean getChildDecision(){
         return this.childDecision;
     }
-    //if the child choose to decide whose parents they want to live with, it will be false
-    //if the child does not want to choose and let parents to decide, it will return true
+    
     public void setChildDecision(boolean dec){
         this.childDecision = dec;
     }
@@ -109,17 +102,15 @@ public class Family extends Civil {
             this.parentDecision = true;
         }
     }
-    //this numChild will decide which custody the judge will choose
-    public void setNumChild (int num){
-      this.numChild = num;
+    public String getSentence(){
+        return sentence;
     }
-    
-    public int getNumChild (){
-      return this.numChild; 
+    public void setNumChild(int n){
+        numChild=n;
     }
-
-    String printInfo() {
-        return "Custody: " + this.custodySentence + "\nFamily Violence: " + this.abuseType; 
+    @Override
+    public String printInfo() {
+        return "Type of case: "+super.getType()+" Type of civil case: "+ super.getTypeCase() +"Abuse Type: "+this.abuseType+ "Sentence: "+this.sentence;
     }
    
     
