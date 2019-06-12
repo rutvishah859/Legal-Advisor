a4aca509f47849c486562c09388b9a24485f5de5
Margarita21552 <50964952+Margarita21552@users.noreply.github.com>
2019-06-11 20:09:21 -0400

    Update Case.java

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/Case.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/Case.java
@@ -6,7 +6,7 @@ The class contains all of the key words for all the classes, and sub classes of 
 package legaladvisor; 
 import java.util.*; 
 
-    public /*abstract*/ class Case {
+    public class Case {
 
     private String remedies;
     private ArrayList <String> type = new ArrayList <String> ();
@@ -21,18 +21,13 @@ import java.util.*;
     private String theft [] = {"stole", "took",  "own"}; 
     private String robbery [] = {"forced","stole","broke","threatened","forcefully stole"}; 
     //CrimeAgainstPerson keywords 
-    private String mensReaTypes[]={"accident","unintended","did not mean to","reckless","risky","ignored","planned","intended","did not know","out of nowhere"};
+    private String mensReaTypes[]={"accident","unintended","did not mean to","reckless","risky","ignored","planned","intended","mistake","out of nowhere"};
     private String crimeTypes2[]={"First Degree Murder","Second Degree Murder","Manslaughter","Physical Assault","Aggravated Assault","Sexual Assault","Verbal Assault"};
     private String murderkeywords []={"assissinated","executed","finished","kill","killed"};
     private String assaultkeywords[]={"assaulted","attacked","beat","hit","hurt","insulted","kicked","punched","raped"};
     //OHRC keywords                     0        1      2       3         4         5          6          7      8        9     10      11     12    13     14           15           16         17          18       19         20        21      22            23       24           25           26             27      28     29       30    31        32      33    34
     private String groundKeywords[]={"black","white","asian","muslim","jewish","christian","buddhist","hindu","female","male","man","woman","girl","boy","pregnancy","citizenship","culture","disabled","mental","physical","disability","gay","homosexual","lesbian","bisexual","transgender","heterosexual","married","kids","children","kid","child","young", "old","wellfare"};
-    
-    
-    //                               0      1       2         3         4       5       6         7       8         9           10          11            12        13      14        15                                
     private String areaKeywords[]={"rent","house","work","employment","hired","fired","member","group","union","hospital","restaurant","movie theater","school","service","place","contract"};
-    
-
     //Charter keywords        0           1        2           3         4       5         6             7          8       9           10         11      12      13    14            15    16     17      18      19       20          21       22     23     24       25     26        27        28        29      30          31         32      33         34       35          36       37     38      38      39      40         41     42         43            44         45                 46           47                                                                   
     private String issues[]={"muslim","jewish","christian","buddhist","hindu","speech","expression","associate","group","non-violent","protest","vote","election","5","parliament","meeting","1","once","travel","Canada","within","throughout","into","out","safety","life","privacy","search","seizure","arrest","detention","arrest","reasons","lawyer","attorney","release","offence","trial","time","guilty","bail","sentence","cruel","unusual","punishment","witness","self-incriminated","interpreter","unequal"};
     //DrugCrime keywords 
@@ -42,13 +37,10 @@ import java.util.*;
     private String jurisdictions[]={"Ontario","Canada"};
     //Family keywords
     private String famKeyword[] = {"abuse","aggreement","child", "custody", "decision","disagree", "divorce", "emotional","marry","mistreatment","permanent", "physical","share", "sexual"};
-    private String emotionalAbuse [] = {"hurting","insults","intimadation", "fun","threats", "yelling"};
-    private String sexualAbuse[] = {"harrassment","touching","unsafe","without consent"};
-    private String physicalAbuse[] = {"burning", "choking", "cutting","hitting","locking","pinching","punching","pushing","slapping","throwing","objects"};
-    //Contract keywords
+    //Contract keywords                   0     1      2     3       4             5           6         7           8       9        10       11       12       13        14        15      16       17          18             
     private String contractWord []= {"agree","break","buy","car","compensate","condition","contract","disagree","failing","goods", "house","illness","make", "mental","negotiate","offer", "pay","purchase","request"};
     private String unlawfulPurpose[] = {"betting", "drugs", "firearm","gambling", "gaming", "kill","prostitution", "prositute"}; //the contract must have lawful objective or purpose
-
+    
     public Case(String rem, String jur){
         this.remedies = rem;
         this.jurisdiction = jur;
@@ -87,7 +79,9 @@ import java.util.*;
         return this.jurisdiction;
     }
     //printInfo is an abstract method which is set by the child/grandchild classes which prints all the info about the case plus the sentance
-    //abstract String printInfo();   
+    public String printInfo(){
+     return  "Jurisdiction: "+ this.jurisdiction +"\nRemedies: "+ this.remedies;   
+    }   
     public boolean SearchMechanism(String keyword, String a[],int n){
         if(a[n].equalsIgnoreCase(keyword)){
             this.setIndexforSearchMechanism(n);
@@ -174,26 +168,23 @@ import java.util.*;
         return famKeyword;
     }
 
+    public void setFamKeyword(String[] famKeyword) {
+        this.famKeyword = famKeyword;
+    }
+
     public String[] getContractWord() {
         return contractWord;
+    }
+
+    public void setContractWord(String[] contractWord) {
+        this.contractWord = contractWord;
     }
 
     public String[] getUnlawfulPurpose() {
         return unlawfulPurpose;
     }
 
-    public String[] getEmotionalAbuse() {
-        return emotionalAbuse;
+    public void setUnlawfulPurpose(String[] unlawfulPurpose) {
+        this.unlawfulPurpose = unlawfulPurpose;
     }
-
-    public String[] getSexualAbuse() {
-        return sexualAbuse;
-    }
-
-    public String[] getPhysicalAbuse() {
-        return physicalAbuse;
-    }
-
-    
-
 }
