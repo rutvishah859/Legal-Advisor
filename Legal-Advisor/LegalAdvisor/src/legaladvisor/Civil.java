b0c8bbacfdbc669a23fdc39ebdf984a245a6ad45
Margarita21552 <50964952+Margarita21552@users.noreply.github.com>
2019-06-11 20:10:30 -0400

    Changed print info

--- Legal-Advisor/LegalAdvisor/src/legaladvisor/Civil.java
+++ Legal-Advisor/LegalAdvisor/src/legaladvisor/Civil.java
@@ -8,16 +8,15 @@ public class Civil extends Case{
         super(rem, jur);
        this.typeCase= tC;
     }
-    //There are two types of Civil case: Family and Contract
     public String getTypeCase(){
         return typeCase;
     }
     public void setTypeCase(String tC){
         this.typeCase= tC;
     }
-//    public String printInfo(){
-//        return super.printInfo() + "Type of civil case: "+this.typeCase;
-//    }
+    public String printInfo(){
+        return  "\nType of civil case: "+this.typeCase+super.printInfo();
+    }
     public void setSentence(String s){
         sentence=s;
     
